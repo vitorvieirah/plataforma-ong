cfd0a1da0bf42294ffee7a3ca8a53994057ae9d8
VitorVieira <vitorhvvieira@gmail.com>
2024-06-03 16:11:24 -0300

    Finalizado a pagina de listagem das ongs, não logada

---
+++ src/main/resources/static/js/listagemONG.js
@@ -0,0 +1,22 @@
+const imgSetaParaCima = '../img/icone-para-cima.png';
+const imgSetaParaBaixo = '../img/icone-para-baixo.png';
+
+function alteraImagemBotaoPesquisa() {
+    let img = document.getElementById('imagem-botao');
+    let imgSrc = img.src;
+
+    if (imgSrc.endsWith('icone-para-cima.png')) {
+        img.src = imgSetaParaBaixo;
+    } else {
+        img.src = imgSetaParaCima;
+    }
+
+    let label = document.getElementById('label-botao-ordenar');
+    let textoAtual = label.innerHTML;
+
+    if(textoAtual === 'A'){
+        label.innerHTML = 'Z';
+    }else{
+        label.innerHTML = 'A';
+    }
+}
