418c8932293245383ff8fcea8bdaa74125116961
VitorVieira <vitorhvvieira@gmail.com>
2024-06-13 16:02:07 -0300

    Adicionando input de fotos e continuando api fetch

--- src/main/resources/static/js/ong.js
+++ src/main/resources/static/js/ong.js
@@ -3,41 +3,15 @@ const URL = "http://localhost:8080/ongs"
 document.getElementById('ongForm').addEventListener('submit', async function(event){
     event.preventDefault();
 
-    let nomeFantasiaIn = document.getElementById('nomeFantasia-ong').value;
-    let nomeEmpresarialIn = document.getElementById('nomeEmpresarial-ong').value;
-    let nomeDonoIn = document.getElementById('nomeDono-ong').value;
-    let facebookIn = document.getElementById('facebook-ong').value;
-    let senhaIn = document.getElementById('senha-ong').value;
-    let instagramIn = document.getElementById('instagram-ong').value; 
-    let xIn = document.getElementById('x-ong').value;
-    let tiktokIn = document.getElementById('tiktok-ong').value;
-    let telefoneIn = document.getElementById('telefone-ong').value;
-    let whatsappIn = document.getElementById('whatsapp-ong').value;
-    let enderecoIn = document.getElementById('endereco-ong').value;
-    let siteOngIn = document.getElementById('siteOng-ong').value;
-    let pixIn = document.getElementById('pix-ong').value;
-    let agenciaIn = document.getElementById('agencia-ong').value;
-    let numContaIn = document.getElementById('numConta-ong').value;
+    let infoOng = localStorage.getItem('info-ongs');
+    
+    if(infoOng){
+        let infoOngJson = JSON.parse(infoOng);
+        let textAreaValue = document.querySelector('textarea').value;
+        infoOngJson.sobreOng = textAreaValue;
 
-    let newOng = {
-        nomeFantasia: nomeFantasiaIn,
-        nomeEmpresarial: nomeEmpresarialIn,
-        nomeDono: nomeDonoIn,
-        facebook: facebookIn,
-        senha: senhaIn,
-        instagram: instagramIn,
-        x: xIn,
-        tiktok: tiktokIn,
-        telefone: telefoneIn,
-        whatsapp: whatsappIn,
-        endereco: enderecoIn,
-        siteOng: siteOngIn,
-        pix: pixIn,
-        agencia: agenciaIn,
-        numConta: numContaIn
-    };
-
-    salvar(newOng);
+        salvar(infoOngJson);
+    }
 });
 
 function logar(){
